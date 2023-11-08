5a3c3b1780d8865cf841f89cd0c4387d3da3ccfd
Time_ON <haas.timeon@gmail.com>
2023-11-08 16:34:32 +0100

    Version 0.0.2 added function to end programm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,43 +16,48 @@ public class Main {
         String authorName;
         int isbnName;
 
-        System.out.println("1 to add to lib, 2 to read lib");
-        int start =sc.nextInt();
+        while (true) {
+            System.out.println("1 to add to lib, 2 to read lib, 3 to exit program");
+            int start = sc.nextInt();
 
-        switch (start) {
-            case 1:
-                System.out.println("Enter the Name of the book");
-                bookName = sc.next();
-                System.out.println("Enter the Name of the author");
-                authorName = sc.next();
-                System.out.println("Enter the isbn of the book");
-                isbnName = sc.nextInt();
-
-                Book book = new Book(bookName,authorName,isbnName);
-
-                while (true) {
-                System.out.println("Enter the Slot to safe book on");
-                selectedSlot = sc.nextInt();
-                selectedSlot--;
-                if (lib1.length >= selectedSlot && selectedSlot >= 0) {
-                    break;
-                } else {
-                    System.out.println("Number is to big");
-                }
+            if (3 == start) {
+                break;
             }
-            addBookToLib(lib1, selectedSlot, book);
 
-            System.out.println(book);
+            switch (start) {
+                case 1:
+                    System.out.println("Enter the Name of the book");
+                    bookName = sc.next();
+                    System.out.println("Enter the Name of the author");
+                    authorName = sc.next();
+                    System.out.println("Enter the isbn of the book");
+                    isbnName = sc.nextInt();
 
-            System.out.println(lib);
-            break;
-            case 2:
-                System.out.println(lib);
-                break;
-            default:
-                System.out.println("Invalid input");
-                break;
+                    Book book = new Book(bookName, authorName, isbnName);
+
+                    while (true) {
+                        System.out.println("Enter the Slot to safe book on");
+                        selectedSlot = sc.nextInt();
+                        selectedSlot--;
+                        if (lib1.length >= selectedSlot && selectedSlot >= 0) {
+                            break;
+                        } else {
+                            System.out.println("Number is to big");
+                        }
+                    }
+                    addBookToLib(lib1, selectedSlot, book);
+
+                    System.out.println(book);
+
+                    System.out.println(lib);
+                    break;
+                case 2:
+                    System.out.println(lib);
+                    break;
+                default:
+                    System.out.println("Invalid input");
+                    break;
+            }
         }
     }
-
 }
